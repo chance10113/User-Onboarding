9cec6d40b6528cb47c1e077d0da4ce4437d127bf
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-01-27 17:45:28 -0600

    started working on App. js, got the states created, and finished the first hjelper function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,46 @@ import Form from './Components/Form.js'
 import schema from "./Validation/FormSchema.js"
 import user from "./Components/User.js"
 
+const initialFormValues = {
+  //Text
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  //Checkbox
+  tOS: false,
+};
+
+const initialFormErrors = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  tOS: false
+}
+
+const initialUsers = [];
+const initialDisabled = true;
+
 function App() {
+  //States 
+  const [users, setUsers] = useState(initialUsers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  //Helper Functions
+
+  const getUsers = () => {
+    axios
+    .get("https://reqres.in/api/users")
+    .then((res) => {
+      setUsers(res.data.data);
+    })
+    .catch((error) => {
+      console.log('getUsers failed', error)
+    })
+  }
   return (
     null
   );
