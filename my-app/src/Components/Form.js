9804b67893af243c5682e7dfbe3a031843f8fe22
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-01-27 15:38:14 -0600

    Finished Form.js, may add additonal inputs outside of MVP later.

--- my-app/src/Components/Form.js
+++ my-app/src/Components/Form.js
@@ -1,17 +1,79 @@
-import React from "react"
+import React from "react";
 
 export default function Form(props) {
-    const { values, submit, change, disabled, errors } = props;
+  const { values, submit, change, disabled, errors } = props;
 
-    const onSubmit = (evt) => {
-        evt.preventDefault();
-        submit();
-    }
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
 
-    const onChange = (evt) => {
-        const { name, value, type, checked } = evt.target;
-        const valueToUse = type === "checkbox" ? checked : value;
-        change(name, valueToUse)
-    };
-    
+  const onChange = (evt) => {
+    const { name, value, type, checked } = evt.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    change(name, valueToUse);
+  };
+
+  return (
+    <form className="user-form container" onSubmit={onSubmit}>
+      <div className="user-form-group submit">
+        <h2> Add User </h2>
+        <button disabled={disabled}>Submit</button>
+        <div className="errors">
+          {/* VALIDATION ERRORS */}
+          <div>{errors.username}</div>
+          <div>{errors.email}</div>
+          <div>{errors.password}</div>
+          <div>{errors.tOS}</div>
+        </div>
+        <div className="user-form-group inputs">
+          <h4> New User Info </h4>
+
+          {/* TEXT INPUTS */}
+
+          <label>
+            Username
+            <input
+              value={values.username}
+              name="username"
+              type="text"
+              onChange={onChange}
+            />
+          </label>
+
+          <label>
+            Email
+            <input
+              value={values.email}
+              name="email"
+              type="text"
+              onChange={onChange}
+            />
+          </label>
+
+          <label>
+            Password
+            <input
+              value={values.password}
+              name="password"
+              type="text"
+              onChange={onChange}
+            />
+          </label>
+
+          {/* Terms Of Service CHECKBOX */}
+
+          <label>
+            I have Read the Terms Of Service
+            <input
+              type="checkbox"
+              name="tOS"
+              checked={values.tOS}
+              onChange={onChange}
+            />
+          </label>
+        </div>
+      </div>
+    </form>
+  );
 }
